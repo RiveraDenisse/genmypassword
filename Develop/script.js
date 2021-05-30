e046c8358a4862a860b0bcb0236dd63c0a4ea8f9
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-05-30 03:00:33 -0500

    added else if statements for options

--- Develop/script.js
+++ Develop/script.js
@@ -11,13 +11,13 @@ var passwordCharacters;
 
 
 //create password begins
-var generatedPassword = function() {
+var generatePassword = function() {
   //ask for length of password
   lengthChar = window.prompt ("Please specify length of password (length must be between 8 and 128) ");
 
     if (lengthChar === "" || lengthChar === null) {
       window.alert("Please provide a value between 8 and 128");
-      return generatedPassword(); 
+      return generatePassword(); 
     }  
 
     else (lengthChar >7 && lengthChar <=128) 
@@ -29,8 +29,8 @@ var generatedPassword = function() {
 //function where user selects character types
 var chooseCharacterTypes = function () {
   
-  capital= window.confirm("Would you like to include capital letters to your password?");
-  var lower = window.confirm("Would you like to include lowecase letters to your password?");
+  var capital= window.confirm("Would you like to include capital letters to your password?");
+  var lower = window.confirm("Would you like to include lowercase letters to your password?");
   var special= window.confirm("Would you like to include special characters to your password?");
   var numbers =window.confirm("Would you like to include numbers to your password?");
 
@@ -38,11 +38,67 @@ var chooseCharacterTypes = function () {
     window.alert ("you must select at least one from the options");
     return chooseCharacterTypes();
   }
-  else if (capital && lower) {
-    passwordCharacters = upperChar() + lowerChar();
+  
+  else if (capital && lower && special && numbers) {
+   passwordCharacters = upperChar() + lowerChar() + specialChar() + numberChar();
     console.log(passwordCharacters);
   }
-  
+  else if (capital && lower && special && !numbers) {
+    passwordCharacters = upperChar() + lowerChar() + specialChar();
+     console.log(passwordCharacters);
+   }
+   else if (capital && lower && !special && numbers) {
+    passwordCharacters = upperChar() + lowerChar() + numberChar();
+     console.log(passwordCharacters);
+   }
+   else if (capital && !lower && special && numbers) {
+    passwordCharacters = upperChar() + specialChar() + numberChar();
+     console.log(passwordCharacters);
+   }
+   else if (!capital && lower && special && numbers) {
+    passwordCharacters = lowerChar() + specialChar() + numberChar();
+     console.log(passwordCharacters);
+   }
+   else if (capital && lower && !special && !numbers) {
+    passwordCharacters = upperChar() + lowerChar();
+     console.log(passwordCharacters);
+   }
+   else if (capital && !lower && special && !numbers) {
+    passwordCharacters = upperChar() + specialChar();
+     console.log(passwordCharacters);
+   }
+   else if (!capital && lower && special && !numbers) {
+    passwordCharacters = lowerChar() + specialChar();
+     console.log(passwordCharacters);
+   }
+   else if (capital && !lower && !special && !numbers) {
+    passwordCharacters = upperChar();
+     console.log(passwordCharacters);
+   }
+   else if (!capital && lower && !special && !numbers) {
+    passwordCharacters = lowerChar();
+     console.log(passwordCharacters);
+   }
+   else if (!capital && !lower && !special && numbers) {
+    passwordCharacters = numberChar();
+     console.log(passwordCharacters);
+   }
+   else if (!capital && !lower && special && !numbers) {
+    passwordCharacters = specialChar();
+     console.log(passwordCharacters);
+   }
+   else if (!capital && lower && !special && numbers) {
+    passwordCharacters = lowerChar() + numberChar();
+     console.log(passwordCharacters);
+   }
+   else if (!capital && !lower && special && numbers) {
+    passwordCharacters = specialChar() + numberChar();
+     console.log(passwordCharacters);
+   }
+   else if (capital && !lower && !special && numbers) {
+    passwordCharacters = upperChar() + numberChar();
+     console.log(passwordCharacters);
+   }
 };
 
  
@@ -50,7 +106,9 @@ var chooseCharacterTypes = function () {
 //**************************************
 //function for uppercase
 function upperChar () {
-  return String.fromCharCode(Math.floor(Math.random()*26)+65);  
+  
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+    
 }
 
 //function for lowercase
@@ -77,28 +135,15 @@ console.log(numberChar());
 
 //iteration
 function pwdg() {
-var password = "";
+var pssword = "";
 for (var i = 0; i< lengthChar; i++) {
   var characterList =passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
-  password= password+characterList;
-  console.log(password);
+  pssword= pssword+characterList;
+  console.log(pssword);
 }
-return password;
+return pssword;
 }
-function generatePassword() {
-  console.log("line86");
-  lengthChar = window.prompt ("Please specify length of password (length must be between 8 and 128) ");
 
-  if (lengthChar === "" || lengthChar === null) {
-    window.alert("Please provide a value between 8 and 128");
-    return generatePassword(); 
-  }  
-
-  else (lengthChar >7 && lengthChar <=128) 
-    window.alert("you have chosen " + lengthChar + " characters");
-console.log("line96");
-    chooseCharacterTypes();  
-}
 
 
  
