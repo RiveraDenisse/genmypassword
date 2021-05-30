f8010bb6338815fe7d0e03fa1857707338972cfe
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-05-30 01:05:59 -0500

    changed variables

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,50 @@ var passwordGen;
 var upperChar;
 var lengthChar;
 var capital;
+var password;
+var passwordCharacters;
 
 
+
+
+//create password begins
+var generatedPassword = function() {
+  //ask for length of password
+  lengthChar = window.prompt ("Please specify length of password (length must be between 8 and 128) ");
+
+    if (lengthChar === "" || lengthChar === null) {
+      window.alert("Please provide a value between 8 and 128");
+      return generatedPassword(); 
+    }  
+
+    else (lengthChar >7 && lengthChar <=128) 
+      window.alert("you have chosen " + lengthChar + " characters");
+      chooseCharacterTypes();      
+    };  
+  
+//-----------------------------------------------------------------------------    
+//function where user selects character types
+var chooseCharacterTypes = function () {
+  
+  capital= window.confirm("Would you like to include capital letters to your password?");
+  var lower = window.confirm("Would you like to include lowecase letters to your password?");
+  var special= window.confirm("Would you like to include special characters to your password?");
+  var numbers =window.confirm("Would you like to include numbers to your password?");
+
+  if (!capital && !lower && !special && !numbers) {
+    window.alert ("you must select at least one from the options");
+    return chooseCharacterTypes();
+  }
+  else if (capital && lower) {
+    passwordCharacters = upperChar() + lowerChar();
+    console.log(passwordCharacters);
+  }
+  
+};
+
+ 
+
+//**************************************
 //function for uppercase
 function upperChar () {
   return String.fromCharCode(Math.floor(Math.random()*26)+65);  
@@ -32,49 +74,41 @@ console.log(lowerChar());
 console.log(upperChar());
 console.log(numberChar());
 
-//create password begins
-var generatePassword = function() {
-  //ask for length of password
-  var lengthChar = window.prompt ("Please specify length of password (length must be between 8 and 128) ");
-
-    if (lengthChar === "" || lengthChar === null) {
-      window.alert("Please provide a value between 8 and 128");
-      return generatePassword(); 
-    }  
-
-    else (lengthChar >7 && lengthChar <=128) 
-      window.alert("you have chosen " + lengthChar + " characters");
-      chooseCapitals();      
-    };  
-  
-//function where user selects character types
-var chooseCapitals = function () {
-  capital= window.confirm("Would you like to include capital letters?");
-
-  //if user confirms then it will go to next question
-  if (capital== true) {
-    passwordGen = upperChar;
-  }
-};
 
 //iteration
-var createPassword = [];
-for (var i = 0; i < lengthChar; i++) {
-  // Generate a random number between 1 and 10
-  // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
-  var passwordGenerated= passwordGen[Math.floor(Math.random()*passwordGen.length)];
-  createPassword.push(passwordGenerated);
-  //password = password +passwordGenerated;
-  
+function pwdg() {
+var password = "";
+for (var i = 0; i< lengthChar; i++) {
+  var characterList =passwordCharacters[Math.floor(Math.random()*passwordCharacters.length)];
+  password= password+characterList;
+  console.log(password);
 }
-  console.log (password, password.length);
+return password;
+}
+function generatePassword() {
+  console.log("line86");
+  lengthChar = window.prompt ("Please specify length of password (length must be between 8 and 128) ");
+
+  if (lengthChar === "" || lengthChar === null) {
+    window.alert("Please provide a value between 8 and 128");
+    return generatePassword(); 
+  }  
+
+  else (lengthChar >7 && lengthChar <=128) 
+    window.alert("you have chosen " + lengthChar + " characters");
+console.log("line96");
+    chooseCharacterTypes();  
+}
+
+
+ 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
-  passwordText.value = password;
+  passwordText.value = pwdg();
 
 }
 
