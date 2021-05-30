f88c6195c580bdee13fc9e90740ea0b5e7c5bb3c
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-05-30 00:59:16 -0500

    created questions and iterations

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordGen;
+var upperChar;
+var lengthChar;
+var capital;
 
 
 //function for uppercase
@@ -31,21 +34,46 @@ console.log(numberChar());
 
 //create password begins
 var generatePassword = function() {
+  //ask for length of password
   var lengthChar = window.prompt ("Please specify length of password (length must be between 8 and 128) ");
+
     if (lengthChar === "" || lengthChar === null) {
       window.alert("Please provide a value between 8 and 128");
-      return generatePassword();
-    }
+      return generatePassword(); 
+    }  
+
+    else (lengthChar >7 && lengthChar <=128) 
+      window.alert("you have chosen " + lengthChar + " characters");
+      chooseCapitals();      
+    };  
+  
+//function where user selects character types
+var chooseCapitals = function () {
+  capital= window.confirm("Would you like to include capital letters?");
+
+  //if user confirms then it will go to next question
+  if (capital== true) {
+    passwordGen = upperChar;
+  }
+};
+
+//iteration
+var createPassword = [];
+for (var i = 0; i < lengthChar; i++) {
+  // Generate a random number between 1 and 10
+  // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
+  var passwordGenerated= passwordGen[Math.floor(Math.random()*passwordGen.length)];
+  createPassword.push(passwordGenerated);
+  //password = password +passwordGenerated;
+  
 }
-
-console.log (generatePassword);
-
+  console.log (password, password.length);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
